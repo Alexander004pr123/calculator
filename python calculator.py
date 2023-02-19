867f4e899eab9e8862b2f778b168974542060259
Alexander004pr123 <99037214+Alexander004pr123@users.noreply.github.com>
2023-02-18 21:12:25 -0400

    Add files via upload

---
+++ python calculator.py
@@ -0,0 +1,9 @@
+num1 = 37 #number 1
+num2 = 23  #number 2
+print(num1+num2) #prints the answer
+print("problem solved") #prints problem solved
+print("thanks for using the calculator") #saying the than
+
+print("////")
+print("/__/")
+print("/_/")
