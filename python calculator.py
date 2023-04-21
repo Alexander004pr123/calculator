15cf1da79a90f8b70ed42a5c4329c3ba48e31372
Alex <99037214+Alexander004pr123@users.noreply.github.com>
2023-04-21 18:55:41 -0400

    Update python calculator.py
    changed the explanation

--- python calculator.py
+++ python calculator.py
@@ -1,7 +1,7 @@
 num1 = 0 #number 1
 num2 = 0  #number 2
-print(num1+num2) #prints the answer
+print(num1+num2) #prints the result (division = / multiplication = * substraction = - addition = +
 print("problem solved") #prints problem solved
-print("thanks for using the calculator") #saying the thanks
+print("thanks for using the calculator") 
 
 
