089354e5b1118b4f204ba1155f7a24026f7b1bbb
Alex <99037214+Alexander004pr123@users.noreply.github.com>
2023-04-21 18:54:46 -0400

    Update python calculator.py
    made the default number 0 of the math

--- python calculator.py
+++ python calculator.py
@@ -1,5 +1,5 @@
-num1 = 37 #number 1
-num2 = 23  #number 2
+num1 = 0 #number 1
+num2 = 0  #number 2
 print(num1+num2) #prints the answer
 print("problem solved") #prints problem solved
 print("thanks for using the calculator") #saying the thanks
